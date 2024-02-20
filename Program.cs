7d4896f6c1eb60b8ba76882de1ee3aa8dc69afe3
t.vlamis <t.vlamis@TECH-BR233>
2024-02-20 17:41:27 +0200

    Assignment2

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using MyErp.Business;
+using MyErp.DAL;
+using MyErp.DAL.Models;
+using MyErp.PrintService;
+
+namespace MyErp.Application;
+
+internal abstract class Program
+{
+    private static void Main()
+    {
+        var orderRepository = new OrderRepository();
+        var customerRepository = new CustomerRepository();
+        var printingService = new PrintingService();
+
+        // Create and save 5 orders
+        for (var i = 0; i < 5; i++)
+        {
+            var order = new Order
+            {
+                OrderId = i + 1, 
+                Amount = (100 * (i + 1)).ToString() 
+            };
+            orderRepository.Create(order);
+        }
+
+
+        // Create and save 5 customers
+        for (var i = 0; i < 5; i++)
+        {
+            var customer = new Customer
+            {
+                CustomerId = i + 1,
+                FirstName = "Dimitris",
+                LastName = "Diamantidis"
+            };
+            customerRepository.Create(customer);
+        }
+
+        // Retrieve all orders and customers
+        var orders = orderRepository.GetAll();
+        var customers = customerRepository.GetAll();
+
+        // Print first order and first customer
+        var firstOrder = orders.FirstOrDefault();
+        var firstCustomer = customers.FirstOrDefault();
+
+        if (firstOrder != null) printingService.Print(firstOrder);
+
+        if (firstCustomer != null) printingService.Print(firstCustomer);
+    }
+}
